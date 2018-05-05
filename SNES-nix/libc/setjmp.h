35a274f1b03d4a92d8186a8af4f623c66a47abf7
InfernoDeity <chorman64@gmail.com>
2018-05-04 20:20:56 -0400

    Update setjmp.h

--- SNES-nix/libc/setjmp.h
+++ SNES-nix/libc/setjmp.h
@@ -2,8 +2,11 @@
 #define __setjmp_h_2018_04_20_12_20
 
 #define setjmp(buf) _setjmp(&buf);
-
-typedef int(*jmp_buf)();
+typedef struct{
+  int(*returnAddress)();
+  void* bp;
+  void* sp;
+}jmp_buf;
 
 int _setjmp(jmp_buf*);
 void longjmp(jmp_buf,int)__attribute__((noreturn));
