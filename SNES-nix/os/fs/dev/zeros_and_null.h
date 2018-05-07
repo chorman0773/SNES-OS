859bdc491932c38545ca6fea622a4c9ac7decc9e
InfernoDeity <chorman64@gmail.com>
2018-05-07 19:11:17 -0400

    Create zeros_and_null.h

---
+++ SNES-nix/os/fs/dev/zeros_and_null.h
@@ -0,0 +1,9 @@
+#ifndef __zeros_and_null_h_2018_05_07_19_09
+#define __zeros_and_null_h_2018_05_07_19_09
+
+void writeNull(unsigned long long,unsigned const char*,unsigned int);
+int readNull(unsigned long long,unsigned char*,unsigned int);
+int readZeros(unsigned long long,unsigned char*,unsigned int);
+
+
+#endif
