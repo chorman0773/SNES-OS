b8501aa4e0442333d2ca9d3259aa47fe96353619
InfernoDeity <chorman64@gmail.com>
2018-05-07 18:12:58 -0400

    Create random.h

---
+++ SNES-nix/os/fs/dev/random.h
@@ -0,0 +1,6 @@
+#ifndef __random_h_2018_05_07_18_12
+#define __random_h_2018_05_07_18_12
+void urandom(unsigned char*,unsigned int);
+void random(unsigned char*,unsigned int);
+void seedUrandom();
+#endif
