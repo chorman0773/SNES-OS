4c0a2af98cbce4eb968168f58c2b5d279b508283
InfernoDeity <chorman64@gmail.com>
2018-05-07 15:44:41 -0400

    Update References.hpp

--- SNES-nix/jvm-rt/References.hpp
+++ SNES-nix/jvm-rt/References.hpp
@@ -24,6 +24,7 @@ namespace java{
         weak_ptr<Handle> ref;
         WeakReference(ReferenceType&,int);
         WeakReference();
+        SoftReference(shared_ptr<Handle>&);
         const Handle* getHandle()const;
         bool isReclaimed()const;
     };
