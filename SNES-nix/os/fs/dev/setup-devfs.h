dcf4f161445f76a4304d4a9bd937248cbd401f07
InfernoDeity <chorman64@gmail.com>
2018-05-08 12:15:35 -0400

    Update setup-devfs.h

--- SNES-nix/os/fs/dev/setup-devfs.h
+++ SNES-nix/os/fs/dev/setup-devfs.h
@@ -2,6 +2,6 @@
 #define __setup_devfs_2018_05_07_19_15
 #include <os/fs/path.h>
 
-void initDevFs(path**);
+void initDevFs(path**,unsigned int(**)(unsigned long long,unsigned char*,unsigned int),void(**)(unsinged long long, unsigned const char*,unsigned int));
 
 #endif
