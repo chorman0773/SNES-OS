86918a6f7cd1f076340dcff8118ac818b8ad07cd
InfernoDeity <chorman64@gmail.com>
2018-05-08 13:00:08 -0400

    Update complex.h

--- SNES-nix/libc/complex.h
+++ SNES-nix/libc/complex.h
@@ -4,16 +4,30 @@
 #define imaginary _Imaginary
 #define complex _Complex
 
-#pragma macros import(macro,"I_val") define(I_val(type))
+#pragma macros import("I_val") define("I_val",type)
 
-#pragma macros deduceType(I_val,type) from(float _Imaginary,double _Imaginary,long double _Imaginary default double _Imaginary)\
-	define I I_val(type)
+#pragma macros deduceType("I",type) from(float _Imaginary,double _Imaginary,long double _Imaginary) default(double _Imaginary)\
+	define("I") I_val(type)
 
 #define _Imaginary_I I_val(float _Imaginary)
 #define _Complex_I 0.0f+I
 
-#define CMPLX(r,i) (double complex)(((double)r)+((double)i)*I)
-#define CMPLXF(r,i) (float complex)(((float)r)+((float)i)*I)
+float complex CMPLXF(float,float);
+double complex CMPLX(double,double);
+long double complex CMPLXL(long double,long double);
+
+float cimagf(float complex);
+double cimag(double complex);
+long double cimagl(long double complex);
+
+float crealf(float complex);
+double creal(double complex);
+long double creall(long double complex);
+
+float complex cprojf(float complex);
+double complex cproj(double complex);
+long double complex cprojl(long double complex);
+
 
 
 #endif
