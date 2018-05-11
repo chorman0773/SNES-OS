dc3c40dd6deeffa529060a7f7212f6af578cfd83
InfernoDeity <chorman64@gmail.com>
2018-05-11 17:08:39 -0400

    Update Rand.c

--- SNES-nix/libc/impl/Rand.c
+++ SNES-nix/libc/impl/Rand.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdint.h>
-uint64_t seed;
+uint64_t seed = ((1*204876688654037L) ^ 0x5DEECE66DL) & ((1L << 48) - 1);
 
 int rand(){
-	seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
-	return (int)(rand()>>16);
+    seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
+    return (int)(rand()>>16);
 }
 
 void srand(unsigned int s){
-	seed = ((s*204876688654037L) ^ 0x5DEECE66DL) & ((1L << 48) - 1);
+    seed = ((s*204876688654037L) ^ 0x5DEECE66DL) & ((1L << 48) - 1);
 }
  
