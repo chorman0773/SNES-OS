8803ec70957fafb199f9c5713a9d595d937e076a
InfernoDeity <chorman64@gmail.com>
2018-05-15 16:01:28 -0400

    Update devices.h

--- MMU/devices.h
+++ MMU/devices.h
@@ -9,4 +9,24 @@ struct device{
 
 device* getDevice(unsigned int);
 
+void init();
+
+int readUrandom(unsigned long long,unsigned char*,unsigned int);
+int readRandom(unsigned long long,unsigned char*,unsigned int);
+
+int writeUrandom(unsigned long long,const unsigned char*,unsigned int);
+int writeRandom(unsigned long long,const unsigned char*,unsigned int);
+
+int readNull(unsigned long long,unsigned char*,unsigned int);
+int writeNull(unsigned long long,const unsigned char*,unsigned int);
+
+int readZeros(unsigned long long,unsigned char*,unsigned int);
+int writeZeros(unsigned long long,const unsigned char*,unsigned int);
+
+int readFull(unsigned long long,unsigned char*,unsigned int);
+int writeFull(unsigned long long,const unsigned char*,unsigned int);
+
+int readAnalog(unsigned long long,unsigned char*,unsigned int);
+int writeAnalog(unsigned long long,const unsigned char*,unsigned int);
+
 #endif
