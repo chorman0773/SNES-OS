25cd17e3d3aea8ece66b8c2fed61c6fb3bc8e481
InfernoDeity <chorman64@gmail.com>
2018-05-22 16:22:28 -0400

    Update Natives.cpp

--- SNES-nix/jvm-rt/Natives.cpp
+++ SNES-nix/jvm-rt/Natives.cpp
@@ -34,7 +34,7 @@ namespace java{
         t.clearMonitor(ref);
         t.setStatus(WAITING);
         t.setHandler(ref);
-        t.nativeWait();//Uses a combination of setjmp and longjmp to go to the next thread until the handler is notified (or the thread is interrupted)
+        t.nativeWait(i.getInt(1));//Uses a combination of setjmp and longjmp to go to the next thread until the handler is notified (or the thread is interrupted)
         if(t.interrupted()){
           i.pushReference(Objects::newInstance(Class("java/lang/InterruptedException")));
           i.dup();
@@ -42,11 +42,47 @@ namespace java{
           handle.invokespecial(i);
           i.throwException();
         }
-        t.setMonitor(ref);
+        t.enterMonitor(ref);
         t.setStatus(NORMAL);
       }
+      static void notify_V(Thread& t,Invocation& i){
+        ReferencePtr ref = i.getReference(0);
+        if(i.getMonitor()!=ref){
+          i.pushReference(Objects::newInstance(Class("java/lang/IllegalMonitorStateException")));
+          i.dup();
+          MethodHandle handle("java/lang/IllegalMonitorStateException","<init>",{"Ljava/lang/String;"},VOID);
+          i.pushReference(StringHelper::fromUtf8("The Current thread does not own this object"));
+          handle.invokespecial(i);
+          i.throwException();
+        }
+        t.notifyWaitingThread(ref);
+      }
+      static void notify_V(Thread& t,Invocation& i){
+        ReferencePtr ref = i.getReference(0);
+        if(i.getMonitor()!=ref){
+          i.pushReference(Objects::newInstance(Class("java/lang/IllegalMonitorStateException")));
+          i.dup();
+          MethodHandle handle("java/lang/IllegalMonitorStateException","<init>",{"Ljava/lang/String;"},VOID);
+          i.pushReference(StringHelper::fromUtf8("The Current thread does not own this object"));
+          handle.invokespecial(i);
+          i.throwException();
+        }
+        t.notifyAllThreads(ref);
+      }
+     static jobject clone_Ljava_lang_Object_(Thread& t,Invocation& i){
+       ReferencePtr ref = i.getReference(0);
+       if(!Objects::instanceof(ref,Class("java/lang/Cloneable")){
+        i.pushReference(Objects::newInstance(Class("java/lang/CloneNotSupportedException")));
+        i.dup();
+        MethodHandle handle("java/lang/CloneNotSupportException","<init>",{},VOID);
+        handle.invokespecial(i);
+        i.throwException();
+       }
+       ReferencePtr ret = Objects::newInstance(h.getType().getClass());
+       Objects::clone(ret,ref);
+       return reinterpret_cast<jobject>(&(*ret));
+     }
     };
   };
-
 
 };
