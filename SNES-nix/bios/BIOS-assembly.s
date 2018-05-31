b0789413212020049793c58d1bfebcd757cd2cd3
InfernoDeity <chorman64@gmail.com>
2018-05-31 14:03:08 -0400

    Update BIOS-assembly.s

--- SNES-nix/bios/BIOS-assembly.s
+++ SNES-nix/bios/BIOS-assembly.s
@@ -6,42 +6,84 @@
 .alias bp  $70:001A
 .alias esp $70:001C
 .alias sp  $70:001E
+.extern hblank
+.extern nmiRoutine
 
-	;RESET
+.section bios_header $01:2000 r
+.db "SNES-OS BIOS" ;Name of the system
+.db "Connor Horman" ;Name of the Author
+.db 0x48 0xac 0x2f 0x74 0x36 0x77 0xe6 0x98 0x19 0xd0 0x96 0xbc 0xa9 0x6e 0x9e ;Developer UUID 
+.db 0x00 0x00 ;Version Code, "1.0"
+
+
+.section header $00:ffc0 r
+.db "SNES-OS BIOS" 
+.db 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
+.db 0x31 0x00 0x00 0x00
+.db 0x00 0x00
+.db 0x00
+.db 0xFF 0xFF
+.db 0x00 0x00
+.reposition $00:ffe4
+.db 0xFF 0xFF
+.db 0x00 0x00
+.db 0x0B 0x00
+.db 0x10 0x00
+.db 0x15 0x00
+.db 0x00 0x00
+.db 0x1A 0x00
+.reposition $00:fff4
+.db 0xFF 0xFF
+.db 0xFF 0xFF
+.db 0xFF 0xFF
+.db 0x00 0x00
+.db 0xFF 0xFF
+.db 0xFF 0xFF
+
+
+.section interrupts $00:0000 rx
+reset:
 	SEI  ;Turn off Interupts
 	CLC
 	XCE ;Go back into regular mode (from emulator mode)
 	REP #$10 ;Set Index Registers to 16-bit mode
 	SEP #$20 ;Set Accumulator Register to 16-bit mode
-	STZ $420b ;Stop any DMA
-	STZ $420c ;Stop any HDMA
-	LDA 0 ;Loop  and clear memory
-reset_loop:
-	TAX
-	EOR A, #$FFFF
-	IFNZ reset_exit
-	STZ $7e:0000,X
-	TXA
-	SUB A, #1
-	BRA reset_loop
-reset_exit:
-	JMP bios ;Jump to the bios
-	
-	
-	;IRQ
-	SEI
-	JMP (long) [$7e:1000]
-	
-	
-	;BRK
+	JMP (long) bios ;Jump to the bios
+brk:
 	SEI
 	JMP (long) [$7e:1004]
 	
-	;NMI
+abort:
+	SEI
+	JMP (long) stop_execute
+	
+nmi:
 	SEI
 	JMP (long) nmiRoutine
+
+irq:
+	SEI
+	LDA $70:8104
+	IFZ +4
+	JMP (long) hblank
+	AND 1
+	IFZ +4
+	JMP (long) [$7e:1000]
+	BRK
 	
-	
+
+
+
+.section bios_functions $fe:0000
+stop: ;GCC-Attribute noret
+	LDA $7e:0000 ;Get Access control
+	IFNZ stop_execute
+	LDA #$0010
+	BRK
+stop_execute:
+	SEI
+	JSL [$7e:1010]
+	STP
 	
 	
 	
@@ -50,7 +92,7 @@ reset_exit:
 	.alias PROC_Exec #$0010
 	
 	;setpermissions(u32 address -> to eax, u16 permissions -> to A)
-	setpermissions:
+setpermissions:
 	LDA $7e:0000 ;Get Access Control
 	CMP A, #$0000 ;Compare to 0
 	IFEQ setpermissions_execute ;If we are being called from the OS then continue
@@ -78,33 +120,18 @@ reset_exit:
 	setpermissions_clearr:
 	JSR MMU_clearprocRead
 	BRA setpermissions_checkw
-	setpermissions_clearw:
+	setpermissions_clearw:syscall
 	JSR MMU_clearprocWrite
 	BRA setpermissions_checkexec
 	setpermissions_clearexec:
 	JSR MMU_clearprocExec
 	BRA setpermissions_end
 	
-	stop: ;GCC-Attribute noret
-	LDA $7e:0000 ;Get Access control
-	IFNZ stop_execute
-	LDA #$0010
-	BRK
-	stop_execute:
-	SEI
-	LDA $7e:1010
-	LDX $7e:1012
-	LDY $7e:1014
-	JSR readSub
-	JSR (long) sp
-	STP
-	
-	.db startupFile 5C 73 79 73 5C 62 6F 6F 66 5C 00
 	
 	
-	bios:
-	;TODO 
-	startup:
+	
+bios:
+	
 	
 	
 	
