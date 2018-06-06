4b1fdf059488568cbb9079fde7d3567ec76dd088
InfernoDeity <chorman64@gmail.com>
2018-06-06 12:53:03 -0400

    Create types.h

---
+++ SNES-nix/os/types.h
@@ -0,0 +1,26 @@
+#ifndef __types_h_2018_06_06_12_49
+#define __types_h_2018_06_06_12_49
+
+typedef int off_t;
+
+typedef int fd_t;
+
+typedef unsigned short pid_t;
+typedef unsigned short uid_t;
+typedef unsigned short gid_t;
+typedef unsigned short devno_t;
+
+typedef void noreturn_t;
+
+#define withc(type) const type
+#define ptr(type) type*
+#define cptr(type) const type*
+
+#ifdef __cplusplus
+#define withv(type) volatile type
+#define withcv(type) const volatile type
+#define vptr(type) volatile type*
+#define cvptr(type) const volatile type*
+#endif
+
+#endif
